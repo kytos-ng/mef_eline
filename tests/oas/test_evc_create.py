94decfcb53e7bf3da7fcdd80d36665dc02b3460c
Luis Marin Vera <luismarin@Luiss-MacBook-Pro.local>
2021-10-14 23:27:34 -0400

    string template

--- tests/oas/test_evc_create.py
+++ tests/oas/test_evc_create.py
@@ -1,3 +1,6 @@
+"""
+EVC creation test
+"""
 import os
 import json
 import requests
@@ -6,10 +9,10 @@ import requests
 def test_required():
     """ test should_fail_due_to_missing_name_attribute_on_payload """
     actual_dir = os.getcwd()
-    json_file = actual_dir+"/tests/oas/evc_params.json"
-    with open(json_file) as f:
-        json_data = json.load(f)
-        f.close()
+    evc_params = actual_dir + "/tests/oas/evc_params.json"
+    with open(evc_params, encoding="utf8") as json_file:
+        json_data = json.load(json_file)
+        json_file.close()
     url = json_data["url"]
     headers = json_data["headers"]
     payload = {
@@ -17,22 +20,23 @@ def test_required():
         "uni_a": json_data["uni_a"],
         "uni_z": json_data["uni_z"],
         "bandwidth": json_data["bandwidth"],
-        "dynamic_backup_path": True
+        "dynamic_backup_path": True,
     }
     request_data = json.dumps(payload)
     response = requests.post(url=url, data=request_data, headers=headers)
     json_response = response.json()
     assert response.status_code == 400
-    assert 'required' in json_response['description']['error_validator']
+    message = json.loads(json_response["description"]).get("message")
+    assert "required" in message["error_validator"]
 
 
-def test_additionalProperties():
+def test_additional_properties():
     """ test should_fail_due_to_additional_properties_on_payload """
     actual_dir = os.getcwd()
-    json_file = actual_dir+"/tests/oas/evc_params.json"
-    with open(json_file) as f:
-        json_data = json.load(f)
-        f.close()
+    evc_params = actual_dir + "/tests/oas/evc_params.json"
+    with open(evc_params, encoding="utf8") as json_file:
+        json_data = json.load(json_file)
+        json_file.close()
     url = json_data["url"]
     headers = json_data["headers"]
     payload = {
@@ -42,23 +46,23 @@ def test_additionalProperties():
         "uni_z": json_data["uni_z"],
         "bandwidth": json_data["bandwidth"],
         "dynamic_backup_path": True,
-        "active": True
+        "active": True,
     }
     request_data = json.dumps(payload)
     response = requests.post(url=url, data=request_data, headers=headers)
     json_response = response.json()
+    message = json.loads(json_response["description"]).get("message")
     assert response.status_code == 400
-    assert 'additionalProperties' in \
-        json_response['description']['error_validator']
+    assert "additionalProperties" in message["error_validator"]
 
 
 def test_pattern():
     """ test should_fail_due_to_invalid_mac_address_on_payload """
     actual_dir = os.getcwd()
-    json_file = actual_dir+"/tests/oas/evc_params.json"
-    with open(json_file) as f:
-        json_data = json.load(f)
-        f.close()
+    evc_params = actual_dir + "/tests/oas/evc_params.json"
+    with open(evc_params, encoding="utf8") as json_file:
+        json_data = json.load(json_file)
+        json_file.close()
     url = json_data["url"]
     headers = json_data["headers"]
     json_data["uni_z"]["interface_id"] = "zz:00:00:00:00:00:00:01:1"
@@ -68,22 +72,23 @@ def test_pattern():
         "uni_a": json_data["uni_a"],
         "uni_z": json_data["uni_z"],
         "bandwidth": json_data["bandwidth"],
-        "dynamic_backup_path": True
+        "dynamic_backup_path": True,
     }
     request_data = json.dumps(payload)
     response = requests.post(url=url, data=request_data, headers=headers)
     json_response = response.json()
+    message = json.loads(json_response["description"]).get("message")
     assert response.status_code == 400
-    assert 'pattern' in json_response['description']['error_validator']
+    assert "pattern" in message["error_validator"]
 
 
 def test_type():
     """ test should_fail_due_to_invalid_mac_address_on_payload """
     actual_dir = os.getcwd()
-    json_file = actual_dir+"/tests/oas/evc_params.json"
-    with open(json_file) as f:
-        json_data = json.load(f)
-        f.close()
+    evc_params = actual_dir + "/tests/oas/evc_params.json"
+    with open(evc_params, encoding="utf8") as json_file:
+        json_data = json.load(json_file)
+        json_file.close()
     url = json_data["url"]
     headers = json_data["headers"]
     payload = {
@@ -91,17 +96,15 @@ def test_type():
         "enabled": True,
         "uni_a": {
             "interface_id": "00:00:00:00:00:00:00:01:1",
-            "tag": {
-                "tag_type": 1,
-                "value": "1"
-            }
+            "tag": {"tag_type": 1, "value": "1"},
         },
         "uni_z": json_data["uni_z"],
         "bandwidth": json_data["bandwidth"],
-        "dynamic_backup_path": True
+        "dynamic_backup_path": True,
     }
     request_data = json.dumps(payload)
     response = requests.post(url=url, data=request_data, headers=headers)
     json_response = response.json()
+    message = json.loads(json_response["description"]).get("message")
     assert response.status_code == 400
-    assert 'type' in json_response['description']['error_validator']
+    assert "type" in message["error_validator"]
