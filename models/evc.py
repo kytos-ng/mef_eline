d77decc56eacebdaec969c81809a5e9610f0e363
Aldo Ortega <aldortega65@gmail.com>
2025-02-23 22:57:24 -0500

    Reduced code in link flaps handling

--- models/evc.py
+++ models/evc.py
@@ -26,7 +26,8 @@ from napps.kytos.mef_eline.exceptions import (ActivationError,
                                               DuplicatedNoTagUNI,
                                               EVCPathNotInstalled,
                                               FlowModException, InvalidPath)
-from napps.kytos.mef_eline.utils import (check_disabled_component,
+from napps.kytos.mef_eline.utils import (_does_uni_affect_evc,
+                                         check_disabled_component,
                                          compare_endpoint_trace,
                                          compare_uni_out_trace, emit_event,
                                          make_uni_list, map_dl_vlan,
@@ -1873,15 +1874,7 @@ class LinkProtection(EVCDeploy):
         """
         if self.is_active():
             return
-        interfaces = (self.uni_a.interface, self.uni_z.interface)
-        if interface not in interfaces:
-            return
-        down_interfaces = [
-            interface
-            for interface in interfaces
-            if interface.status != EntityStatus.UP
-        ]
-        if down_interfaces:
+        if not _does_uni_affect_evc(self, interface, "up"):
             return
         if self.try_to_handle_uni_as_link_up(interface):
             return
@@ -1891,7 +1884,7 @@ class LinkProtection(EVCDeploy):
                 'status': interface.status.value,
                 'status_reason': interface.status_reason,
             }
-            for interface in interfaces
+            for interface in (self.uni_a.interface, self.uni_z.interface)
         }
         try:
             self.try_to_activate()
@@ -1913,22 +1906,15 @@ class LinkProtection(EVCDeploy):
         """
         if not self.is_active():
             return
-        interfaces = (self.uni_a.interface, self.uni_z.interface)
-        if interface not in interfaces:
-            return
-        down_interfaces = [
-            interface
-            for interface in interfaces
-            if interface.status != EntityStatus.UP
-        ]
-        if not down_interfaces:
+        if not _does_uni_affect_evc(self, interface, "down"):
             return
         interface_dicts = {
             interface.id: {
                 'status': interface.status.value,
                 'status_reason': interface.status_reason,
             }
-            for interface in down_interfaces
+            for interface in (self.uni_a.interface, self.uni_z.interface)
+            if interface.status != EntityStatus.UP
         }
         self.deactivate()
         log.info(
