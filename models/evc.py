f5acd9e6cb681043f097e067c016e43bb0e8c19c
Vinicius Arcanjo <vindasil@fiu.edu>
2025-10-15 16:51:14 -0300

    chore: removed log.info that could turn out noisy

--- models/evc.py
+++ models/evc.py
@@ -1242,7 +1242,6 @@ class EVCDeploy(EVCBase):
         """Prepare flows to install UNIs."""
         uni_flows = {}
         if not path:
-            log.info("install uni flows without path.")
             return uni_flows
 
         # Determine VLANs
